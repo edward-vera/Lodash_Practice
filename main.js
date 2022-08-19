075d6684f7d326d5656cf00f5ffd9d43f2a88550
edward-vera <103536879+edward-vera@users.noreply.github.com>
2022-08-18 20:47:08 -0500

    final commit with tests

--- main.js
+++ main.js
@@ -10,3 +10,43 @@ const myWithout = _.without(numbers, 8, 16, 40, 64);
 console.log(myWithout)
 const myShuffle = _.shuffle(numbers);
 console.log(myShuffle)
+
+
+// TESTS
+
+const assert = require('assert');
+
+if(typeof describe === 'function') {
+
+
+    describe("#testing chunk length", function() {
+        it('should divide the array into 4 arrays', function(){
+            assert.equal(myChunk.length, 3);
+        });
+    })
+
+
+
+    describe("#testing if numbers are in revers", function() {
+        it('should reverse the array of numbers', function(){
+            assert.equal(myReverse[0], 72);
+        });
+    })
+
+
+    describe("#testing chunk length", function() {
+        it('should check if numbers have been removed', function(){
+            assert.equal(
+                JSON.stringify(myWithout),
+                JSON.stringify([72,56,48,32,24])
+            );
+        });
+    })
+
+    describe("#testing chunk length", function() {
+        it('should check if numbers are not in the same order', function(){
+            assert.notEqual(myChunk, myShuffle); 
+        });
+    })
+
+}
